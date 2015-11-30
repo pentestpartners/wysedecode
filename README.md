921629408b02c67fe74d2efad6a98154b7e0e81e
dave <dave>
2015-11-30 12:08:16 +0000

    Added initial code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wysedecode
-Code to decode Wyse passwords
+Some very simple and hacked together C to decode a stored Wyse password. This is pretty much a direct translation from the assembly language and probably needs translation to a different language.
+
+It's released under the MIT licence, so feel free to steal it, but be nice and attribute other's work.
---
+++ pwdecode.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void NFuseDecode(unsigned char *src, int length, unsigned char *dst)
+{
+   unsigned char a;
+   int i;
+
+   length/=2;
+
+   for (i=0; i<length; i++)
+   {
+      a=src[i*2];
+      a-=1;
+      a<<=4;
+      a+=src[i*2+1];
+      a-=0x41;
+      dst[i]=a;
+   }
+
+   for (i=length; i>=0; i--)
+   {
+      a=dst[i-1];
+      a^=dst[i];
+      a^=0xA5;
+      dst[i]=a;
+   }
+   dst[length]='\0';
+}
+
+void main(int argc, char **argv)
+{
+   unsigned char buffer[256];
+   int i;
+
+   NFuseDecode(argv[1], strlen(argv[1]), buffer);
+   printf("%s\n", buffer);
+}
